42aead2c117da047ce435f966e3a9183280d6f67
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-18 20:29:10 -0300

    Component Card

---
+++ stories/Card.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { Card } from '../src/components/Card';
+
+export default {
+  title: 'components/Card',
+  component: Card,
+} as ComponentMeta<typeof Card>;
+
+export const CardComponent = () => {
+    return (<Card>
+        <h1>Olá, eu sou um card</h1>
+    </Card>)
+}
