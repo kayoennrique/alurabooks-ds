be851168a3bc8891a0b2dd21a6dc056d94711478
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-19 17:44:33 -0300

    Adding AbButton variants

--- src/components/AbButton/index.tsx
+++ src/components/AbButton/index.tsx
@@ -1,21 +1,38 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const StylizedButton = styled.button`
-    background: #EB9B00;
+export interface AbButtonProps {
+    text?: string
+    kind?: 'primary' | 'secondary'
+    onClick?: () => void
+}
+
+const StylizedButton = styled.button<AbButtonProps>`
+    background: ${(props: AbButtonProps) => props.kind === 'primary' ? '#EB9B00' : '#FFF'};
     padding: 16px 32px;
     border: 2px solid #EB9B00;
-    color: #FFF;
+    color: ${(props: AbButtonProps) => props.kind === 'primary' ? '#FFF' : '#EB9B00'};
     font-size: 20px;
     cursor: pointer;
-    &:hover {
-        background: #B87900;
-        border: 2px solid #B87900;
+    ${(props: AbButtonProps) => props.kind === 'primary'
+        ? css`
+            &:hover {
+                background: #B87900;
+                border: 2px solid #B87900;
+            }
+        `
+        : css`
+        &:hover {
+            background: #FFF;
+            border: 2px solid #B87900;
+            color: #B87900;
+        }
+        `
     }
-`;
-
-export const AbButton = () => {
-    return (<StylizedButton>
-        Clique aqui!
+`
+export const AbButton = ({ text, onClick, kind = 'primary' }: AbButtonProps) => {
+    return (<StylizedButton onClick={onClick} kind={kind}>
+        {text}
     </StylizedButton>)
 }
+
--- stories/AbButton.stories.tsx
+++ stories/AbButton.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AbButton } from '../src'
+import { AbButton, AbButtonProps } from '../src'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
@@ -7,6 +7,18 @@ export default {
     component: AbButton
 } as ComponentMeta<typeof AbButton>
 
-const Template: ComponentStory<typeof AbButton> = () => <AbButton />
+const Template: ComponentStory<typeof AbButton> = (args) => <AbButton {...args}/>
 
 export const Primary = Template.bind({})
+
+Primary.args = {
+    text: 'Ab Botão Primário',
+    kind: 'primary'
+} as AbButtonProps
+
+export const Secondary = Template.bind({})
+
+Secondary.args = {
+    text: 'Ab Botão Secundário',
+    kind: 'secondary'
+} as AbButtonProps
